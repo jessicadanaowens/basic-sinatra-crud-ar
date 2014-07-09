9540489ce8c909ca0ac2f9d9c0c0caeff5155d2e
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-09 11:50:29 -0600

    registration form

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -7,3 +7,12 @@ feature "homepage" do
     expect(page).to have_selector(:link_or_button, 'Register')
   end
 end
+
+feature "register" do
+  scenario "vew registration form" do
+    visit "/"
+    click_button "Register"
+
+    expect(page).to have_content ("username password")
+  end
+end
