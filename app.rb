a9f5478bc5ea77fcb551816eb880e87fbf61e62c
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-09 16:16:54 -0600

    user login auth error

--- app.rb
+++ app.rb
@@ -1,18 +1,74 @@
 require "sinatra"
+require "rack-flash"
 require "active_record"
 require "./lib/database_connection"
 
+
 class App < Sinatra::Application
+  enable :sessions
+  use Rack::Flash
+
   def initialize
     super
     @database_connection = DatabaseConnection.new(ENV["RACK_ENV"])
   end
 
   get "/" do
-    erb :homepage
+    if session[:id]
+      name = @database_connection.sql("SELECT username from users where id = '#{session[:id].first.values.first.to_i}';")
+      erb :logged_in, :locals=>{:name=>name}
+    else
+      erb :homepage
+    end
+
   end
 
   get "/register" do
     erb :register
   end
-end
+
+  get "/login" do
+    erb :login
+  end
+
+  get "/logout" do
+    session.clear
+    redirect '/'
+  end
+
+  post "/register" do
+    check_reg(params[:username], params[:password])
+
+    end
+
+  post "/sessions" do
+    session[:id] = (@database_connection.sql("SELECT id from users where username = '#{params[:username]}';"))
+    redirect '/'
+  end
+
+  private
+
+  def check_reg(username, password)
+    if username == ""
+      flash[:notice] = "Please enter a username"
+      redirect "/register"
+    elsif password == ""
+      flash[:notice] = "Please enter a password"
+      redirect "/register"
+      #go into database and return all of the usernames, which will give us an array of hashes
+      #pull out the names and if the names include username, throw an error
+    elsif (@database_connection.sql("SELECT username from users")).select {|user| user[:username] == username } == []
+      @database_connection.sql("INSERT INTO users (username, password) VALUES ('#{params[:username]}', '#{params[:password]}')")
+      flash[:notice] = "Thank you for registering"
+      redirect "/"
+    else
+      flash[:notice] = "That username is already taken"
+      redirect "/register"
+    end
+  end
+
+  end
+
+
+
+
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,18 +1,61 @@
 require 'spec_helper'
 
+def authenticate_user
+  if fill_in('username', :with => '')
+    expect(page).to have_content ("Please enter a username")
+  elsif fill_in('password', :with => '')
+    expect(page).to have_content ("Please enter a password")
+  end
+end
+
 feature "homepage" do
   scenario "view registration button" do
     visit "/"
-
+    expect(page).to have_selector(:link_or_button, 'Log In')
     expect(page).to have_selector(:link_or_button, 'Register')
   end
 end
 
-feature "register" do
-  scenario "vew registration form" do
+feature "register form" do
+  scenario "view registration form" do
     visit "/"
     click_button "Register"
 
     expect(page).to have_content ("username password")
   end
 end
+
+feature "registered" do
+  scenario "click register then see welcome message on the homepage" do
+    visit "/register"
+
+    click_button "Register"
+
+    expect(page).to have_content ("Thank you for registering")
+  end
+end
+
+feature "login and Logout" do
+  scenario "fills in username and password and logs in" do
+    visit "/login"
+
+    expect(page).to have_content ("username password")
+
+    click_button "Log In"
+    expect(page).to have_content ("Welcome")
+
+    expect(page).to have_selector(:link_or_button, 'Log Out')
+  end
+end
+
+feature "user authenication" do
+  scenario "if username or password fields are blank, flash message" do
+    visit "/regiser"
+
+    authenticate_user
+  end
+end
+
+
+
+
